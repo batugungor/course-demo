c7ca160157da8d7a54c59e4c7c618675bfdf9303
Batuhan Gungor <58569386+batugungor@users.noreply.github.com>
2025-04-04 15:24:32 +0200

    Delete nwire-dev-learn-management-system-development directory

--- nwire-dev-learn-management-system-development/config/twig.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-add_filter('timber/loader/loader', function ($loader) {
-    $loader->addPath(NWIRE_DEV_LEARN_MANAGEMENT_SYSTEM_PATH . '/views', NWIRE_DEV_LEARN_MANAGEMENT_SYSTEM_NAME);
-    return $loader;
-});
--- nwire-dev-learn-management-system-development/src/Enums/ProgressStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Nwire_Dev\LearnManagementSystem\Enums;
-
-enum ProgressStatus: int
-{
-    case InProgress = 0;
-    case Finished = 1;
-}
